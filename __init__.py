bfe3cae3e49dd28bc046be81c1519e8b2f8488e2
DonutsDelivery <megusta52@proton.me>
2025-04-07 20:19:46 +0200

    Update __init__.py
    fixed init

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .Donut_Detailer import NODE_CLASS_MAPPINGS as m1
 from .Donut_Detailer_2 import NODE_CLASS_MAPPINGS as m2
 from .Donut_Detailer_4 import NODE_CLASS_MAPPINGS as m3
-from .Donut_Detailer_LoRA_6 import NODE_CLASS_MAPPINGS as m4
+from .Donut_Detailer_6 import NODE_CLASS_MAPPINGS as m4
 
 NODE_CLASS_MAPPINGS = {**m1, **m2, **m3, **m4}
 __all__ = ["NODE_CLASS_MAPPINGS"]
