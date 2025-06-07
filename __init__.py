a283aaadf97e558dd999dcf9a0d3489078a874fb
DonutsDelivery <megusta52@proton.me>
2025-06-07 07:35:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,8 +6,6 @@ from .DonutDetailer5        import NODE_CLASS_MAPPINGS as m4
 from .DonutDetailerXLBlocks import NODE_CLASS_MAPPINGS as m5
 from .DonutClipEncode       import NODE_CLASS_MAPPINGS as m6
 from .DonutWidenMerge       import NODE_CLASS_MAPPINGS as m7
-from .DonutLoadUNetModels   import NODE_CLASS_MAPPINGS as m8
-from .DonutLoadCLIPModels   import NODE_CLASS_MAPPINGS as m9
 
 # new LoRA nodes (include display names)
 from .donut_lora_nodes      import NODE_CLASS_MAPPINGS        as m_lora
@@ -16,7 +14,7 @@ from .donut_lora_nodes      import NODE_DISPLAY_NAME_MAPPINGS as d_lora
 # build globals
 NODE_CLASS_MAPPINGS = {
     **m1, **m2, **m3, **m4, **m5,
-    **m6, **m7, **m8, **m9,
+    **m6, **m7,
     **m_lora,
 }
 
